e2e571422c62bacd3c75c8492a294b9e8c51f7bd
j.halish richard <halishrichard5@gmail.com>
2022-12-12 19:32:25 +0530

    nevin

--- 14a_Write a program to read data from the given excel file and explore various commands.py
+++ 14a_Write a program to read data from the given excel file and explore various commands.py
@@ -1,8 +1,12 @@
 import pandas as pd
  #ttt
 # Reading the CSV file
+
 df = pd.read_csv("Iris.csv")
- 
+
+
+# for open the file in text mode by using next step
+#df = pd.read_csv("Iris.txt")
 # Printing top 5 rows
 df.head()
 df.shape
