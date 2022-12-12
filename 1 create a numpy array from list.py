dfd54ecfc38c33e4dddf2323479c8477474dd141
j.halish richard <halishrichard5@gmail.com>
2022-12-12 21:03:15 +0530

    special

--- 1 create a numpy array from list.py
+++ 1 create a numpy array from list.py
@@ -12,3 +12,17 @@ print ("List: ", lst)
 
 # displaying array
 print ("Array: ", arr)
+
+
+def solve(nums):
+   for i in range(max(nums)+1):
+      count=0
+      for j in nums:
+         if j >= i:
+            count+=1
+      if count == i:
+         return i
+      return -1
+
+nums = [4,6,7,7,1,0]
+print(solve(nums))
